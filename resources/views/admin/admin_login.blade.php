299378c4ce81067cec0ba5a3e7423a2f4c8a0757
h2vag <h2vag@CEL>
2023-04-30 10:13:36 +0800

    login alert message

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="css/adminLoginStyle.css">
     <title>Admin Login</title>
 </head>
@@ -15,18 +18,18 @@
         <img src="/resource/logo_version1.png" alt="logo" class="logo">
     </nav>
     <div class="container">
+        @if(Session::has('success'))
+            <div class="alert alert-success fade-in-out">{{Session::get('success')}}</div>
+            @endif
+            @if(Session::has('fail'))
+            <div class="alert alert-danger fade-in-out">{{Session::get('fail')}}</div>
+            @endif
         <div class="content-container">
             <div class="image-container">
                 <img class="img" src="/resource/cinema.png" alt="">
                 <h2>Sign in to your account</h2>
             </div>
             <form action="{{route('login-user')}}" method="POST">
-            @if(Session::has('success'))
-                <div class="alert alert-success">{{Session::get('success')}}</div>
-                @endif
-                @if(Session::has('fail'))
-                <div class="alert alert-danger">{{Session::get('fail')}}</div>
-                @endif
                 @csrf
                  <h1>LOGIN</h1>
                  <div class="input-box">
