a6fa3496473797a3d9ca07f4dd2641661fffa92c
Celeste Vagilidad <113239413+cjoyjoooy@users.noreply.github.com>
2023-05-19 02:21:30 +0800

    Update ShowTimesController.php

--- app/Http/Controllers/ShowTimesController.php
+++ app/Http/Controllers/ShowTimesController.php
@@ -14,25 +14,8 @@ class ShowTimesController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index($id)
+    public function index()
     {
-        $movies = Movie::find($id);
-
-        $locationIds = Schedule::where('movie_id', $id)
-            ->distinct('location_id')
-            ->pluck('location_id');
-        $locationNames = Location::whereIn('id', $locationIds)->get();
-
-        $dates = Schedule::whereIn('location_id', $locationIds)
-            ->select('date_schedule','location_id')
-            ->distinct('date_schedule')
-            ->where('movie_id', $id)
-            ->get();
-        $schedules = Schedule::whereIn('location_id', $locationIds)
-            ->select('date_schedule','time_start','location_id','id')
-            ->where('movie_id', $id)
-            ->get();
-        return view('/client/clientshowlistPage', compact('movies', 'locationIds', 'schedules', 'locationNames','dates'));
     }
     public function show($id)
     {
@@ -52,6 +35,6 @@ class ShowTimesController extends Controller
             ->select('date_schedule','time_start','location_id','id')
             ->where('movie_id', $id)
             ->get();
-        return view('/client/clientshowlistPage', compact('movies', 'locationIds', 'schedules', 'locationNames','dates'));
+        return view('client.clientshowlistPage', compact('movies', 'locationIds', 'schedules', 'locationNames','dates'));
     }
 }
